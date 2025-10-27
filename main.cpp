99713f4466572fa40d069a7790a99c846ac2b443
Edward Choi <edwardchoi2001@gmail.com>
2025-10-26 17:57:11 -0700

    first edit after cloning

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>  
+#include <string>   
+#include <vector>   
+#include <list>     
+#include <set>
+#include <algorithm> 
+#include <iomanip>  
+#include <iterator> 
 using namespace std;
 
 int main() {
+    vector<string> my_vector;
+    list<string> my_list;
+    set<string> my_set;
     
 
     return 0;
